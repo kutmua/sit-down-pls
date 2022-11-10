b4aa71d40a311b7d2b9f7b558976fa9293add38e
Mustafa Kutlimetov <kutlimetov98@bk.ru>
2022-11-10 19:12:41 +0300

    modified breadcrumbs, create product page

---
+++ src/js/blocks-scripts/swiper-product-card.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const swiperSmall = new Swiper(".swiper-small", {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  const swiperBig = new Swiper(".swiper-big", {
+    spaceBetween: 10,
+    thumbs: {
+      swiper: swiperSmall,
+    },
+  });
+})
--- src/js/main.js
+++ src/js/main.js
@@ -11,4 +11,52 @@ document.addEventListener('DOMContentLoaded', function(){
       // console.log(event.currentTarget.nextElementSibling.nextElementSibling);
     })
   })
+
+
+  /* -------------------------- */
+  // TODO: реализовать эту функцию для всех кнопок КУПИТЬ
+  const productPage = 'product-card.html';
+  const btnsBuy = document.querySelectorAll('.catalog-card__btn');
+
+  btnsBuy.forEach(btn => {
+    btn.addEventListener('click', (event) => {
+      let productType = event.target.previousSibling.previousElementSibling.previousElementSibling.textContent.trim().slice(1, length-1);
+      localStorage.setItem('breadСrumbs', JSON.stringify(productType));
+    })
+  })
+
+  function productCheck() {
+    let fileName = location.href.split("/").slice(-1);
+    if (fileName[0] === productPage) {
+      createBreadCrumbs();
+    }
+  }
+
+  function createBreadCrumbs() {
+    let productType = JSON.parse(localStorage.getItem('breadСrumbs'));
+
+    const list = document.querySelector('.bread-crumbs__list');
+      const listItem = document.createElement('li');
+        const itemLink = document.createElement('a');
+
+    const currentItemLink = document.querySelectorAll('.bread-crumbs__item-link');
+    currentItemLink.forEach(link => {
+      if (link.classList.contains('bread-crumbs__item-link_active')) {
+        link.classList.remove('bread-crumbs__item-link_active')
+      }
+    })
+
+    /* присваение классов */
+    listItem.classList.add('bread-crumbs__list-item');
+      itemLink.classList.add('bread-crumbs__item-link', 'another-links', 'bread-crumbs__item-link_active')
+
+    /* отрисовка элементов */
+    itemLink.textContent = productType;
+    listItem.append(itemLink);
+    list.append(listItem);
+
+  }
+
+  productCheck()
+
 });
